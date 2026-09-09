bdf353078a443541b96ec3a99cd226ddd2eb3235
Artur Bańka <artur.banka@gmail.com>
2026-09-09 22:51:43 +0200

    refactor(landing-page): remove BetaTestSection component from landing page and delete associated file

--- apps/landing-page/src/pages/PLandingPage.tsx
+++ apps/landing-page/src/pages/PLandingPage.tsx
@@ -1,5 +1,4 @@
 import { AboutFeedntSection } from '@/components/_organisms/OAboutFeedntSection.tsx';
-import { BetaTestSection } from '@/components/_organisms/OBetaTestSection.tsx';
 import { SeeYouSoonSection } from '@/components/_organisms/OSeeYouSoonSection.tsx';
 import { WhatFeedntIsNotSection } from '@/components/_organisms/OWhatFeedntIsNotSection.tsx';
 import { WhatFeedntIsSection } from '@/components/_organisms/OWhatFeedntIsSection.tsx';
@@ -15,7 +14,6 @@ export function LandingPage() {
         <WhatFeedntIsNotSection />
         <WhatFeedntIsSection />
         <AboutFeedntSection />
-        <BetaTestSection />
         <SeeYouSoonSection />
       </main>
     </>
